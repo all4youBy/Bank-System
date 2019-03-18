a0b9a565ca331a39ec1cdfc38e6c6ea6fd468da1
Maxim <max281@icloud.com>
2019-03-18 13:46:36 +0300

    add entities and relations

---
+++ bank-system/src/main/java/com/alekhnovich/bsu/fpmi2019/banksystem/respository/BankAccountCustom.java
@@ -0,0 +1,9 @@
+package com.alekhnovich.bsu.fpmi2019.banksystem.respository;
+
+import com.alekhnovich.bsu.fpmi2019.banksystem.models.BankAccount;
+
+import java.util.List;
+
+public interface BankAccountCustom {
+    List<BankAccount> getAllbankAccounts();
+}
---
+++ bank-system/src/main/java/com/alekhnovich/bsu/fpmi2019/banksystem/respository/impl/BankAccountCustomImpl.java
@@ -0,0 +1,37 @@
+package com.alekhnovich.bsu.fpmi2019.banksystem.respository.impl;
+
+import com.alekhnovich.bsu.fpmi2019.banksystem.models.BankAccount;
+import com.alekhnovich.bsu.fpmi2019.banksystem.respository.BankAccountCustom;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Repository
+public class BankAccountCustomImpl implements BankAccountCustom {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public BankAccountCustomImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public List<BankAccount> getAllbankAccounts() {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<BankAccount> query = cb.createQuery(BankAccount.class);
+        Root<BankAccount> root = query.from(BankAccount.class);
+        query.select(root);
+        TypedQuery<BankAccount> q = entityManager.createQuery(query);
+        return q.getResultList();
+
+    }
+}
